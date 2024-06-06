703d3c34e58e45ccc38041e8a8a4bc7c8991e3cc
Invior <twhlost@gmail.com>
2024-06-06 18:41:06 +0400

    Скорректировал название папки с pages на Pages и заменил соответствующие импорты

--- src/components/AppRoutes/AppRoutes.jsx
+++ src/components/AppRoutes/AppRoutes.jsx
@@ -1,28 +1,28 @@
 import { Routes, Route } from "react-router-dom";
 import { appRoutes } from "../../lib/appRoutes";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
-import HomePage from "../../pages/Home/HomePage";
-import TaskPage from "../../pages/Task/TaskPage";
-import ExitPage from "../../pages/Exit/ExitPage";
-import AddTaskPage from "../../pages/AddTask/AddTaskPage";
-import SignInPage from "../../pages/SignInPage/SignInPage";
-import SignUpPage from "../../pages/SignUpPage/SignUpPage";
-import NotFoundPage from "../../pages/NotFound/NotFoundPage";
+import HomePage from "../../Pages/Home/HomePage";
+import TaskPage from "../../Pages/Task/TaskPage";
+import ExitPage from "../../Pages/Exit/ExitPage";
+import AddTaskPage from "../../Pages/AddTask/AddTaskPage";
+import SignInPage from "../../Pages/SignInPage/SignInPage";
+import SignUpPage from "../../Pages/SignUpPage/SignUpPage";
+import NotFoundPage from "../../Pages/NotFound/NotFoundPage";
 
 export const AppRoutes = () => {
 
-    return (
+  return (
     <Routes>
-        <Route element={<PrivateRoute />}>
-          <Route path={appRoutes.HOME} element={<HomePage />}>
-            <Route path={appRoutes.TASK} element={<TaskPage />} />
-            <Route path={appRoutes.EXIT} element={<ExitPage />} />
-            <Route path={appRoutes.ADD_TASK} element={<AddTaskPage />} />
-          </Route>
+      <Route element={<PrivateRoute />}>
+        <Route path={appRoutes.HOME} element={<HomePage />}>
+          <Route path={appRoutes.TASK} element={<TaskPage />} />
+          <Route path={appRoutes.EXIT} element={<ExitPage />} />
+          <Route path={appRoutes.ADD_TASK} element={<AddTaskPage />} />
         </Route>
-        <Route path={appRoutes.SIGNIN} element={<SignInPage />} />
-        <Route path={appRoutes.SIGNUP} element={<SignUpPage />} />
-        <Route path={appRoutes.NOT_FOUND} element={<NotFoundPage />} />
-      </Routes>
-    )
+      </Route>
+      <Route path={appRoutes.SIGNIN} element={<SignInPage />} />
+      <Route path={appRoutes.SIGNUP} element={<SignUpPage />} />
+      <Route path={appRoutes.NOT_FOUND} element={<NotFoundPage />} />
+    </Routes>
+  )
 }
